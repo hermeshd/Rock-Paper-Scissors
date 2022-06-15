6cc66fedbc0f2ffc3db9fcba9e6b44e8f8420be3
Hermes <hermesd.guerrero@gmail.com>
2022-06-14 23:36:02 -0500

    Finish script for the actual game to happen Code for selecting a winner and also inputting the player's player
    Took me a while but I'm quite proud

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
-var game = [
-    "Rock",
-    "Paper",
-    "Scissors"
+var options = [
+    "rock",
+    "paper",
+    "scissors"
 ];
 
+//Function to define what the computer plays
 function computerPlay(arr) {
-    var play = arr[Math.floor(Math.random() * arr.length)];
-    console.log(play);
+    var move = arr[Math.floor(Math.random() * arr.length)];
+    console.log("PC plays: " + move);
+    return move;
 }
 
-computerPlay(game);
+//Function to define which one wins
+function playGame(playerMove, pcMove) {
+    switch (playerMove) {
+        case "rock":
+            if (pcMove === "rock") {
+                console.log("It's a draw :D");
+                break;
+            } else if (pcMove === "scissors") {
+                console.log("You win! Rock beats Scissors!");
+                break;
+            } else if (pcMove === "paper") {
+                console.log("You lose! Paper beats Rock!");
+                break;
+            }
+        case "paper":
+            if (pcMove === "rock") {
+                console.log("You win! Paper beats Rock!");
+                break;
+            } else if (pcMove === "scissors") {
+                console.log("You lose! Scissors beat Paper");
+                break;
+            } else if (pcMove === "paper") {
+                console.log("It's a draw :D");
+                break;
+            }
+        case "scissors":
+            if (pcMove === "rock") {
+                console.log("You lose! Rock beats Scissors");
+                break;
+            } else if (pcMove === "scissors") {
+                console.log("It's a draw :D");
+                break;
+            } else if (pcMove === "paper") {
+                console.log("You win! Scissors beat Paper");
+                break;
+            }
+    }
+}
+
+var input = prompt("Please make your move: ");
+
+if (input.toLowerCase() !== "rock" && input.toLowerCase() !== "paper" && input.toLowerCase() !== "scissors" ) {
+    alert("Invalid play");
+}
+
+playGame(input.toLowerCase(), computerPlay(options));
