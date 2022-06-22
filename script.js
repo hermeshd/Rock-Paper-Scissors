d5a9fa778b753f24c2f1f61582490b954f9cd157
Hermes <hermesd.guerrero@gmail.com>
2022-06-22 15:16:29 -0500

    Modify game to play the best out of 5 rounds

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ var options = [
     "scissors"
 ];
 
+//Counter of how many times has a player won
+var HumanWins = 0;
+var PCWins = 0;
+
 //Function to define what the computer plays
 function computerPlay(arr) {
     var move = arr[Math.floor(Math.random() * arr.length)];
@@ -23,17 +27,21 @@ function playGame(playerMove, pcMove) {
                 break;
             } else if (pcMove === "scissors") {
                 console.log("You win! Rock beats Scissors!");
+                HumanWins = HumanWins + 1;
                 break;
             } else if (pcMove === "paper") {
                 console.log("You lose! Paper beats Rock!");
+                PCWins = PCWins + 1;
                 break;
             }
         case "paper":
             if (pcMove === "rock") {
                 console.log("You win! Paper beats Rock!");
+                HumanWins = HumanWins + 1;
                 break;
             } else if (pcMove === "scissors") {
                 console.log("You lose! Scissors beat Paper");
+                PCWins = PCWins + 1;
                 break;
             } else if (pcMove === "paper") {
                 console.log("It's a draw :D");
@@ -42,21 +50,20 @@ function playGame(playerMove, pcMove) {
         case "scissors":
             if (pcMove === "rock") {
                 console.log("You lose! Rock beats Scissors");
+                PCWins = PCWins + 1;
                 break;
             } else if (pcMove === "scissors") {
                 console.log("It's a draw :D");
                 break;
             } else if (pcMove === "paper") {
                 console.log("You win! Scissors beat Paper");
+                HumanWins = HumanWins + 1;
                 break;
             }
     }
 }
 
-
-
-
-
+//Loop to play the game up to five times
 function bestOutOfThree() {
     for (let i = 0; i < 5; i++) {
         
@@ -67,6 +74,22 @@ function bestOutOfThree() {
         }
 
         playGame(input.toLowerCase(), computerPlay(options));
+
+        if (HumanWins === 3) {
+            console.log("The Human is the winner");
+            console.log(HumanWins);
+            console.log(PCWins);
+            break;
+        }
+        
+        if (PCWins === 3) {
+            console.log("The PC is the winner")
+            console.log(HumanWins);
+            console.log(PCWins);
+            break;
+        }
+
+        
     }
 }
 
